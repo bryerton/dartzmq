4061a97f29a8e8b9f8667838bc9b7a6d69be1e37
Bryerton Shaw <bryerton@gmail.com>
2025-02-05 11:47:30 -0800

    added setOptionBool()

--- lib/src/socket.dart
+++ lib/src/socket.dart
@@ -244,6 +244,16 @@ class ZBaseSocket {
     _checkReturnCode(result, ignore: [EINTR]);
   }
 
+ 
+  void setOptionBool(final int option, final bool value) {
+    final Pointer<Uint32> ptr = malloc.allocate(4);
+    ptr.value = (value) ? 1 : 0;
+    final result = _bindings.zmq_setsockopt(
+        _socket, option, ptr.cast<Uint8>(), 4);
+    malloc.free(ptr);
+    _checkReturnCode(result, ignore: [EINTR]);
+  }
+   
   /// Sets the socket's long term secret key.
   /// You must set this on both CURVE client and server sockets, see zmq_curve(7).
   /// You can provide the [key] as a 40-character string encoded in the Z85 encoding format.
